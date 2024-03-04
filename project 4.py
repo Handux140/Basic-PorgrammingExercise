e0034665650b793f57ff4c4fd241ce2db097486d
Handux140 <160829278+Handux140@users.noreply.github.com>
2024-03-04 17:08:58 +0700

    Update project 4.py

--- project 4.py
+++ project 4.py
@@ -1,8 +1,8 @@
 # take two strings as input
-string1= input("4")
-strings2=input("8")
+string1= input("string1")
+strings2=input("string2")
 # concatenate the strings
-results_string= 4 +8
+results_string= "string1 + string2"
 
 # print the resulting string
 print("concatented string:",results_string)
